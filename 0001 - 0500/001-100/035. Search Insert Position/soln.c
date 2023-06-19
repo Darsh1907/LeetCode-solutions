126c008705e83dd5db04ad7198d60c5bed3e7ffb
Darsh Patel <darshpatelisin@gmail.com>
2023-06-19 10:47:40 +0530

    provided complete code for 35. Search Insert Position

--- 0001 - 0500/001-100/035. Search Insert Position/soln.c
+++ 0001 - 0500/001-100/035. Search Insert Position/soln.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 int searchInsert(int* nums, int numsSize, int target) {
   int low = 0;
   int high = numsSize - 1;
@@ -9,4 +11,17 @@ int searchInsert(int* nums, int numsSize, int target) {
     else high = mid - 1;
   }
   return low;
+}
+
+int main() {
+    // Example usage
+    int nums[] = {1, 3, 5, 6};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int target = 5;
+
+    int index = searchInsert(nums, numsSize, target);
+    
+    printf("Insert position: %d\n", index);
+    
+    return 0;
 }
