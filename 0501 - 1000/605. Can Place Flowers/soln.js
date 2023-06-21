f8d07547d26d087787e3c81c4a94d20dd4136115
Darsh Patel <darshpatelisin@gmail.com>
2023-06-21 13:22:15 +0530

    nothing

--- 0501 - 1000/605. Can Place Flowers/soln.js
+++ 0501 - 1000/605. Can Place Flowers/soln.js
@@ -2,12 +2,12 @@
 
 var canPlaceFlowers = function(flowerbed, n) {
     
-    for(let i = 0; i < flowerbed.length; i++) {
+    for(let i=0; i<flowerbed.length; i++) {
         if(flowerbed[i] === 0) {
-            if((flowerbed[i-1] === 0 && flowerbed[i+1] === 0) || 
-               (flowerbed[i-1] === undefined && flowerbed[i+1] === 0) || 
-                (flowerbed[i+1] === undefined && flowerbed[i-1] === 0) || 
-                (flowerbed[i-1] === undefined && flowerbed[i+1] === undefined && flowerbed[i] === 0)) {
+            if((flowerbed[i-1]===0 && flowerbed[i+1]===0) || 
+               (flowerbed[i-1]===undefined && flowerbed[i+1]===0) || 
+                (flowerbed[i+1]===undefined && flowerbed[i-1]===0) || 
+                (flowerbed[i-1]===undefined && flowerbed[i+1]===undefined && flowerbed[i]===0)) {
 
                 flowerbed[i] = 1;
                 n--;
