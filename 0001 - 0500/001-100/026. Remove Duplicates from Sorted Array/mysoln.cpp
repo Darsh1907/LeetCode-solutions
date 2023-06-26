b830c20dfba22781936cd3a979e9e8e2a4e8f2b2
Darsh Patel <darshpatelisin@gmail.com>
2023-06-26 21:50:47 +0530

    added README for 26

--- 0001 - 0500/001-100/026. Remove Duplicates from Sorted Array/mysoln.cpp
+++ 0001 - 0500/001-100/026. Remove Duplicates from Sorted Array/mysoln.cpp
@@ -7,9 +7,7 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         for(int i=0; i<(nums.size()-1); i){
-            if(nums[i]==nums[i+1]){
-                nums.erase(nums.begin()+(i+1));
-            }
+            if(nums[i]==nums[i+1]) nums.erase(nums.begin()+(i+1));
             else i++;
         }
         return nums.size();
@@ -21,13 +19,11 @@ int main() {
     vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
     Solution solution;
     int length = solution.removeDuplicates(nums);
-    
     cout << "New length: " << length << endl;
     cout << "Modified array: ";
     for (int i = 0; i < length; i++) {
         cout << nums[i] << " ";
     }
     cout << endl;
-    
     return 0;
 }
