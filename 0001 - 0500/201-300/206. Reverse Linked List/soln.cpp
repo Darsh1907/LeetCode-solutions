a7b9b1043b453dc938db897c308376dd055cb3a5
Darsh Patel <darshpatelisin@gmail.com>
2023-06-30 10:46:15 +0530

    Update soln.cpp

--- 0001 - 0500/201-300/206. Reverse Linked List/soln.cpp
+++ 0001 - 0500/201-300/206. Reverse Linked List/soln.cpp
@@ -13,14 +13,12 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
     ListNode* prev = nullptr;
-
     while (head) {
       ListNode* next = head->next;
       head->next = prev;
       prev = head;
       head = next;
     }
-
     return prev;
     }
 };
