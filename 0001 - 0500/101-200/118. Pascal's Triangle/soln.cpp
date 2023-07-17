a0a9a688bdb6f8210aa7618d779c5182efe0d168
Darsh Patel <darshpatelisin@gmail.com>
2023-07-17 12:00:59 +0530

    modified 118

--- 0001 - 0500/101-200/118. Pascal's Triangle/soln.cpp
+++ 0001 - 0500/101-200/118. Pascal's Triangle/soln.cpp
@@ -6,7 +6,6 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> output;
-
         for (int i=0; i<numRows; i++) {
             vector<int> temp(i+1, 1);
             for(int j=1; j<i; j++){
