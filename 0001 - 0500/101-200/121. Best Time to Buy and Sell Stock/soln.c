20844fc20ab1409a500679e78828a154a4eefb71
Darsh Patel <darshpatelisin@gmail.com>
2023-07-24 10:58:58 +0530

    121 using C

--- 0001 - 0500/101-200/121. Best Time to Buy and Sell Stock/soln.c
+++ 0001 - 0500/101-200/121. Best Time to Buy and Sell Stock/soln.c
@@ -2,9 +2,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int result = 0, l = 0, r = 0;
-        while(r<prices.size()){
+        while (r<prices.size()){
             if(prices[r]>prices[l]) result=max(result, prices[r]-prices[l]);
-            else l=r;
+            else l = r;
             r++;
         }
         return result;
