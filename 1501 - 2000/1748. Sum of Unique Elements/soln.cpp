c91e3bfbb1cec1552e5b6e97edf93f3cae4d6b88
Darsh Patel <darshpatelisin@gmail.com>
2023-08-26 22:17:25 +0530

    solved 1748

---
+++ 1501 - 2000/1748. Sum of Unique Elements/soln.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        unordered_map<int, int> umap;
+        int result = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(umap.find(nums[i]) == umap.end()) umap[nums[i]]=1;
+            else umap[nums[i]]++;
+        }
+        unordered_map<int, int>::iterator itr;
+        for(itr = umap.begin(); itr!=umap.end(); itr++){
+            if(itr->second == 1) result=result+(itr->first);
+        }
+        return result;
+    }
+};
