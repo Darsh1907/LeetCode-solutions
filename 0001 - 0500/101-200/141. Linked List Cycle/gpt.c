9701114f29571c64b2501f379223bf5cab95248c
Darsh Patel <darshpatelisin@gmail.com>
2023-09-04 19:06:15 +0530

    minor changes

--- 0001 - 0500/101-200/141. Linked List Cycle/gpt.c
+++ 0001 - 0500/101-200/141. Linked List Cycle/gpt.c
@@ -17,10 +17,7 @@ bool hasCycle(struct ListNode *head) {
     while (fast != NULL && fast->next != NULL) {
         slow = slow->next;      // Move the slow pointer by one step.
         fast = fast->next->next; // Move the fast pointer by two steps.
-
-        if (slow == fast) {
-            return true; // If they meet, there is a cycle.
-        }
+        if(slow == fast) return true; // If they meet, there is a cycle.
     }
 
     return false; // If fast reaches the end of the list, there is no cycle.
