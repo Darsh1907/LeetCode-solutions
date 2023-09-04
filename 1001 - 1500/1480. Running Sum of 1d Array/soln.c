dcc92492222ae3ba284f27d37c690706fe81b66d
Darsh Patel <darshpatelisin@gmail.com>
2023-09-04 18:26:15 +0530

    412, 876, 1342, 1480

--- 1001 - 1500/1480. Running Sum of 1d Array/soln.c
+++ 1001 - 1500/1480. Running Sum of 1d Array/soln.c
@@ -1,10 +1,21 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* runningSum(int* nums, int numsSize, int* returnSize){
-    int arr[numsSize];
-    for (int i=1; i++; i==numsSize)
-    {
-        if (arr[i-1]==0)
-            arr[i] = nums[i];
-        else arr[i] = nums[i-1]+nums[i];
+    if (numsSize <= 1) {
+        *returnSize = numsSize;
+        return nums;
     }
-    return arr;
+    int* result = (int*)malloc(numsSize * sizeof(int)); // Allocate memory for the result array
+    if (result == NULL) {
+        // Handle memory allocation error
+        *returnSize = 0;
+        return NULL;
+    }
+    result[0] = nums[0];
+    for (int i = 1; i < numsSize; i++) {
+        result[i] = result[i - 1] + nums[i];
+    }
+    *returnSize = numsSize;
+    return result;
 }
