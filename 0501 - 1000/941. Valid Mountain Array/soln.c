5318de335e7c89be82130251c52229ac4d649add
Darsh Patel <darshpatelisin@gmail.com>
2023-09-10 11:18:38 +0530

    Create soln.c

---
+++ 0501 - 1000/941. Valid Mountain Array/soln.c
@@ -0,0 +1,16 @@
+bool validMountainArray(int* arr, int arrSize){
+    if(arrSize<3 || arr[0]>arr[1]) return false;
+    bool increase=true; 
+    int i=0;
+    while(i<arrSize-1) {
+        if(arr[i]==arr[i+1]) return false;
+        if(increase) {
+            if(arr[i]>arr[i+1])
+                increase=false;
+        }
+        else if(arr[i]<arr[i+1]) return false;
+        i++;
+    }
+    if(increase) return false;
+    return true;
+}
