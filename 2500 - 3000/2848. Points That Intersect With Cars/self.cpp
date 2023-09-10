10cbf1f21ae8dec00d1d6bed219405aea290bea2
Darsh Patel <darshpatelisin@gmail.com>
2023-09-10 10:08:14 +0530

    2848, 2849, 2850

---
+++ 2500 - 3000/2848. Points That Intersect With Cars/self.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        vector<bool> points(100, false);
+        for(int i=0; i<nums.size(); i++){
+            for(int j=nums[i][0]; j<=nums[i][1]; j++){
+                points[j-1] = true;
+            }
+        }
+        int count = 0;
+        for(int i=0; i<points.size(); i++){
+            if(points[i]==true) count++;
+        }
+        return count;
+    }
+};
