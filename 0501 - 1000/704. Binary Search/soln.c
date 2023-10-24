a4d309d9dd984a75f3e923b0d6a88ac730c1609a
Darsh Patel <darshpatelisin@gmail.com>
2023-10-24 09:48:48 +0530

    solved 704, 2529

---
+++ 0501 - 1000/704. Binary Search/soln.c
@@ -0,0 +1,12 @@
+int search(int* nums, int numsSize, int target){
+    int left = 0;
+    int right = numsSize-1;
+    int mid = 0;
+    while( left<=right) {
+        mid=(left+right)/2;
+        if(target > nums[mid]) left = mid+1;
+        else if(target < nums[mid]) right = mid-1;
+        else return mid;
+    }
+    return -1;
+}
