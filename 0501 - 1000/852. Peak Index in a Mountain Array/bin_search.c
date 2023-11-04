e5ad9429f84111fe5d419eaa4f7f69da63dd9afd
Darsh Patel <darshpatelisin@gmail.com>
2023-11-05 00:29:01 +0530

    solved 852

---
+++ 0501 - 1000/852. Peak Index in a Mountain Array/bin_search.c
@@ -0,0 +1,10 @@
+int peakIndexInMountainArray(int* arr, int arrSize) {
+    int s = 0;
+    int e = arrSize-1;
+    while(s<e){
+        int mid = s+(e-s)/2;
+        if(arr[mid]<arr[mid+1]) s=mid+1;
+        else e=mid;
+    }
+    return s;
+}
