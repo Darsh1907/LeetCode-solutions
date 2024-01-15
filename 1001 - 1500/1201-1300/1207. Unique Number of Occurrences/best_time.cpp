ed7904277f7def6e5712132557132944cd58aac9
Darsh Patel <darshpatelisin@gmail.com>
2024-01-15 18:20:57 +0530

    readmes

--- 1001 - 1500/1201-1300/1207. Unique Number of Occurrences/best_time.cpp
+++ 1001 - 1500/1201-1300/1207. Unique Number of Occurrences/best_time.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& a) {
         unordered_map<short, short> mp;
-        for(int i = 0; i < a.size(); i++)
-            mp[a[i]]++;
+        for(int i = 0; i < a.size(); i++) mp[a[i]]++;
         set<short> st;
-        for(auto i: mp)
-            st.insert(i.second);
+        for(auto i: mp) st.insert(i.second);
         return st.size() == mp.size();
     }
 };
