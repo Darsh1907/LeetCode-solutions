ebfb862f8c774007554ab7c74be5a2e05ab609cc
Darsh Patel <darshpatelisin@gmail.com>
2024-01-16 10:39:56 +0530

    448

---
+++ 0001 - 0500/401-500/448. Find All Numbers Disappeared in an Array/set.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n = nums.size();
+        set<int> s;
+        for(int i=0; i<n; i++) s.insert(nums[i]);
+        vector<int> result;
+        for(int i=1; i<=n; i++){
+            if(s.find(i) == s.end()) result.push_back(i);
+        }
+        return result;
+    }
+};
