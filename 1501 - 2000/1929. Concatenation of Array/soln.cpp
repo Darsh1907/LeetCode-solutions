bed5e6bb2a93520f8e24d8d295e9ad36a965107e
Darsh Patel <darshpatelisin@gmail.com>
2024-01-21 01:22:15 +0530

    Update soln.cpp

--- 1501 - 2000/1929. Concatenation of Array/soln.cpp
+++ 1501 - 2000/1929. Concatenation of Array/soln.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int size = nums.size();
-        for(int i=0; i<size; i++){
-            nums.push_back(nums[i]);
-        }
+        for(int i=0; i<size; i++) nums.push_back(nums[i]);
         return nums;
     }
 };
