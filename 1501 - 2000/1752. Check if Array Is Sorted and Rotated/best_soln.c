11acf97c1d75d8f1a1ba73ce030ba5f98e517b62
Darsh Patel <darshpatelisin@gmail.com>
2024-01-21 01:20:46 +0530

    Update best_soln.c

--- 1501 - 2000/1752. Check if Array Is Sorted and Rotated/best_soln.c
+++ 1501 - 2000/1752. Check if Array Is Sorted and Rotated/best_soln.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+#include <stdbool.h>
+
 bool check(int* nums, int numsSize) {
     int count = 0;
     for(int i=1; i<numsSize; i++){
@@ -6,4 +9,13 @@ bool check(int* nums, int numsSize) {
     }
     if(nums[numsSize-1]>nums[0]) count++;
     return count<=1;
+}
+
+int main(){
+    int arr1[5] = {3,4,5,1,2};
+    int arr2[4] = {2,1,3,4};
+    int arr3[3] = {1,2,3};
+    printf("%d\n", check(arr1, 5));
+    printf("%d\n", check(arr2, 4));
+    printf("%d", check(arr3, 3));
 }
