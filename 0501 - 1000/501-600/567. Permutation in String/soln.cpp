9562c5b534d7de9b7b60c1dd0830b98145d457b7
Darsh Patel <darshpatelisin@gmail.com>
2024-01-26 12:43:44 +0530

    Update soln.cpp

--- 0501 - 1000/501-600/567. Permutation in String/soln.cpp
+++ 0501 - 1000/501-600/567. Permutation in String/soln.cpp
@@ -11,8 +11,9 @@ public:
 
         for (int i=s1.size(); i<s2.size(); i++) {
             if (count == temp) return true;
-            temp[s2[i-s1.size()] - 'a']--;
-            temp[s2[i] - 'a']++;
+            // Implementation of sliding window
+            temp[s2[i-s1.size()] - 'a']--; // remove count of first element that will get removed from the sliding window
+            temp[s2[i] - 'a']++; // add count of new element that will come into the sliding window.
         }
 
         return count == temp; // Check for the last window
