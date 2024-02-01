4adc199fba8a4551aa9589657ed0cf81434285fb
Darsh Patel <darshpatelisin@gmail.com>
2024-02-01 18:46:14 +0530

    solve 54 and 1152

---
+++ 0001 - 0500/001-100/051-075/54. Spiral Matrix/better_time.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int l = matrix[0].size();
+        int top = 0, right = l - 1, bottom = n - 1, left = 0, k = 0;
+        vector<int> arr;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) arr.push_back(matrix[top][i]);
+            top++;
+            for (int i = top; i <= bottom; i++) arr.push_back(matrix[i][right]);
+            right--;
+            if (top <= bottom) {
+                for(int i = right; i >= left; i--) arr.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+            if (left <= right) {
+                for(int i = bottom; i >= top; i--) arr.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+        return arr;
+    }
+};
