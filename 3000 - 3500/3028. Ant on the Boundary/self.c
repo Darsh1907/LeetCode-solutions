ec3f08792c72c7a6b1e074001469ec72ebc8db74
Darsh Patel <darshpatelisin@gmail.com>
2024-02-04 14:00:04 +0530

    Update self.c

--- 3000 - 3500/3028. Ant on the Boundary/self.c
+++ 3000 - 3500/3028. Ant on the Boundary/self.c
@@ -1,6 +1,5 @@
 int returnToBoundaryCount(int* nums, int numsSize) {
-    int count = 0;
-    int sum = 0;
+    int count=0, sum=0;
     for(int i=0; i<numsSize; i++){
         sum = sum + nums[i];
         if(sum==0) count++;
