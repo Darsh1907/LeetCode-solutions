946d3aa67bd32920a78e49813dbdd8cfc63e4b58
Darsh Patel <darshpatelisin@gmail.com>
2024-02-11 10:23:15 +0530

    Update best_time.cpp

--- 0001 - 0500/201-300/205. Isomorphic Strings/best_time.cpp
+++ 0001 - 0500/201-300/205. Isomorphic Strings/best_time.cpp
@@ -4,7 +4,7 @@ public:
         if(s.size()!=t.size()) return false;
         int hash[256] = {0};
         bool ismap[256] = {0};
-        for(int i=0;i<s.size();i++){
+        for(int i=0; i<s.size(); i++){
             if(hash[s[i]] == 0 && !ismap[t[i]]){
                 hash[s[i]] = t[i];
                 ismap[t[i]] = true;
