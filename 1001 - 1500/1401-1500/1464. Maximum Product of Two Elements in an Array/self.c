46a9264cd226b77ab4e5ecb5d4a63170bdca9153
Darsh Patel <darshpatelisin@gmail.com>
2024-02-12 21:27:38 +0530

    1464

---
+++ 1001 - 1500/1401-1500/1464. Maximum Product of Two Elements in an Array/self.c
@@ -0,0 +1,12 @@
+int maxProduct(int* nums, int numsSize) {
+    int max = nums[0];
+    int sec_max = 0;
+    for(int i=1; i<numsSize; i++){
+        if(nums[i]>max) {
+            sec_max = max;
+            max = nums[i];
+        }
+        else if(nums[i]>sec_max) sec_max = nums[i];
+    }
+    return (max-1)*(sec_max-1);
+}
