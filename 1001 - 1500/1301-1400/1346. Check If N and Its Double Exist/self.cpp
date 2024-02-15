6d9d60315aa5c8a884831162ac7bcf4c2bbdc662
Darsh Patel <darshpatelisin@gmail.com>
2024-02-15 07:11:49 +0530

    Update self.cpp

--- 1001 - 1500/1301-1400/1346. Check If N and Its Double Exist/self.cpp
+++ 1001 - 1500/1301-1400/1346. Check If N and Its Double Exist/self.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        set<int> s;
+        unordered_set<int> s;
         for(int i=0; i<arr.size(); i++){
             if(s.find(arr[i]*2) != s.end()) return true;
             if(arr[i]%2==0 && s.find(arr[i]/2) != s.end()) return true;
