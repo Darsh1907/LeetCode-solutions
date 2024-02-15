f692c3749f7bd2914f6e37ee08428a67eab8c39b
Darsh Patel <darshpatelisin@gmail.com>
2024-02-15 08:14:37 +0530

    2656

--- 2500 - 3000/2656. Maximum Sum With Exactly K Elements/self.c
+++ 2500 - 3000/2656. Maximum Sum With Exactly K Elements/self.c
@@ -1,18 +1,7 @@
-int find_max(int* nums, int numsSize){
+int maximizeSum(int* nums, int numsSize, int k){
     int max = nums[0];
     for(int i=1; i<numsSize; i++){
         if(nums[i]>max) max = nums[i];
     }
-    return max;
-}
-
-int maximizeSum(int* nums, int numsSize, int k){
-    int max_ele = find_max(nums, numsSize);
-    int sum = 0;
-    while(k) {
-        sum = sum + max_ele;
-        max_ele = max_ele + 1;
-        k--;
-    }
-    return sum;
+    return k*max + (k-1)*(k)/2;
 }
