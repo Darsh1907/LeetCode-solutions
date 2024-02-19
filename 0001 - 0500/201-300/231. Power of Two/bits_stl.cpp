c3a78bf1a7897d48e448992da4e4eccb55832f2a
Darsh Patel <darshpatelisin@gmail.com>
2024-02-19 10:53:43 +0530

    solve 231

---
+++ 0001 - 0500/201-300/231. Power of Two/bits_stl.cpp
@@ -0,0 +1,11 @@
+// It is known that a perfect square will have only 1 high bit in its binary form.
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<0)return false;
+        int bits=__builtin_popcount(n);
+        if(bits==1) return true;
+        return false;
+    }
+};
