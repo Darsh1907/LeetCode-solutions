68598d12eb23447786c261abdf75923c5def19cc
Darsh Patel <darshpatelisin@gmail.com>
2024-03-04 18:29:17 +0530

    Update self.cpp

--- 2001 - 2500/2390. Removing Stars From a String/self.cpp
+++ 2001 - 2500/2390. Removing Stars From a String/self.cpp
@@ -3,7 +3,7 @@ public:
     string removeStars(string s) {
         string result = "";
         for(int i=0; i<s.size(); i++){
-            if(s[i]=='*' && result.size()!=0) result.pop_back();
+            if(s[i]=='*') result.pop_back();
             else result.push_back(s[i]);
         }
         return result;
