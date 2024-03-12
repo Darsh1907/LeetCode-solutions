23496f5b0f13d1713cf388bd2bef8c31f77a8f6a
Darsh Patel <darshpatelisin@gmail.com>
2024-03-12 20:00:23 +0530

    modifications

--- 0001 - 0500/301-400/383. Ransom Note/count_array.cpp
+++ 0001 - 0500/301-400/383. Ransom Note/count_array.cpp
@@ -3,9 +3,7 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         if (ransomNote.length() > magazine.length()) return false;
         int alphaCount[26] = {0};
-        for(char ch : magazine) {
-            alphaCount[ch - 'a']++;
-        }
+        for(char ch : magazine) alphaCount[ch - 'a']++;
         for(char ch : ransomNote) {
             if(alphaCount[ch - 'a']-- <= 0) return false;
         }
--- 0001 - 0500/301-400/383. Ransom Note/count_array2.cpp
+++ 0001 - 0500/301-400/383. Ransom Note/count_array2.cpp
@@ -2,14 +2,8 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int count[256] = {0};
-        for(const auto &m : magazine)
-        {
-            ++count[m];
-        }
-        for(const auto &r : ransomNote)
-        {
-            if(--count[r] < 0) return false;
-        }
+        for(const auto &m : magazine) ++count[m];
+        for(const auto &r : ransomNote) if(--count[r] < 0) return false;
         return true;
     }
 };
