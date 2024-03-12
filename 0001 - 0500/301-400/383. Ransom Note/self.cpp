52cce3ab76d0174b1444bd491ada3784f8582c54
Darsh Patel <darshpatelisin@gmail.com>
2024-03-12 20:02:53 +0530

    Update self.cpp

--- 0001 - 0500/301-400/383. Ransom Note/self.cpp
+++ 0001 - 0500/301-400/383. Ransom Note/self.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> umap;
-        for(int i=0; i<magazine.size(); i++) {
-            if(umap.find(magazine[i]) == umap.end()) umap[magazine[i]]=1;
-            else umap[magazine[i]]++;
-        }
+        for(int i=0; i<magazine.size(); i++) umap[magazine[i]]++;
         for(int i=0; i<ransomNote.size(); i++) {
             if(umap.find(ransomNote[i]) == umap.end()) return false;
             else if(umap[ransomNote[i]] == 0) return false;
