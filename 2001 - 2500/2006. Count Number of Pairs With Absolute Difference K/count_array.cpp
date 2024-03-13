790de206ff304a79d1a34f888439d10c5ca5ccda
Darsh Patel <darshpatelisin@gmail.com>
2024-03-13 07:01:15 +0530

    minor changes 2006

--- 2001 - 2500/2006. Count Number of Pairs With Absolute Difference K/count_array.cpp
+++ 2001 - 2500/2006. Count Number of Pairs With Absolute Difference K/count_array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> v(101);
         int count=0;
         for(int i=0; i<nums.size(); i++) v[nums[i]]++;    
-        for(int i=k; i<101; i++) count+=v[i]*v[i-k];
+        for(int i=k; i<101; i++) count += v[i]*v[i-k];
         return count;
     }
 };
