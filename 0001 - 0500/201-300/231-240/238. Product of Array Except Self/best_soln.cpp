f9cdd2f40f9737ed8355797bb7e81cca91030d9b
Darsh Patel <darshpatelisin@gmail.com>
2024-03-15 07:03:11 +0530

    Update best_soln.cpp

--- 0001 - 0500/201-300/231-240/238. Product of Array Except Self/best_soln.cpp
+++ 0001 - 0500/201-300/231-240/238. Product of Array Except Self/best_soln.cpp
@@ -4,14 +4,12 @@ public:
         int n = nums.size(), total = 1;
         vector<int> result(n, 1);
         //left side
-        for(int i=1; i<n; i++){
-            result[i] = result[i-1]*nums[i-1];
-        }
+        for(int i=1; i<n; i++) result[i] = result[i-1]*nums[i-1];
         //right side
         int right = 1;
         for(int i=n-2; i>=0; i--){
-            right*=nums[i+1];
-            result[i] *= right;
+            right = right * nums[i+1];
+            result[i] = result[i] * right;
         }
         return result;
     }
