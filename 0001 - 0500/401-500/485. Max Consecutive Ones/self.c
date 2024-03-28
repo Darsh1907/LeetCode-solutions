154a6124de4a6b5db5c6708acc8972faaa7da7af
Darsh Patel <darshpatelisin@gmail.com>
2024-03-28 22:38:29 +0530

    solve 485

--- 0001 - 0500/401-500/485. Max Consecutive Ones/self.c
+++ 0001 - 0500/401-500/485. Max Consecutive Ones/self.c
@@ -1,14 +1,10 @@
 int findMaxConsecutiveOnes(int* nums, int n){
     int count=0;
     int maxcount=0;
-    for(int i=0;i<n;i++)
-    {
-        if(nums[i]==1)
-        count++;
-        else
-        count=0;
-        if(count>maxcount)
-        maxcount=count;
+    for(int i=0; i<n; i++) {
+        if(nums[i]==1) count++;
+        else count=0;
+        if(count>maxcount) maxcount=count;
     }
     return maxcount;
 }
