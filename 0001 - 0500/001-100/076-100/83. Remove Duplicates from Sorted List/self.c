c9260358884f13d7ff56e06742ee6759261ef7f5
Darsh Patel <darshpatelisin@gmail.com>
2024-03-29 10:55:18 +0530

    solve 83

---
+++ 0001 - 0500/001-100/076-100/83. Remove Duplicates from Sorted List/self.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+*/
+
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if(head==NULL || head->next==NULL) return head;
+    struct ListNode* itr = head;
+    while(itr->next != NULL){
+        if(itr->val == itr->next->val) {
+            itr->next = itr->next->next;
+        }
+        else {
+            itr = itr->next;
+        }
+    }
+    return head;
+}
