3e6749eab8b561156806377969d4981545c909b8
Darsh Patel <darshpatelisin@gmail.com>
2024-04-04 16:59:31 +0530

    Update self.c

--- 0001 - 0500/001-100/076-100/83. Remove Duplicates from Sorted List/self.c
+++ 0001 - 0500/001-100/076-100/83. Remove Duplicates from Sorted List/self.c
@@ -10,12 +10,8 @@ struct ListNode* deleteDuplicates(struct ListNode* head) {
     if(head==NULL || head->next==NULL) return head;
     struct ListNode* itr = head;
     while(itr->next != NULL){
-        if(itr->val == itr->next->val) {
-            itr->next = itr->next->next;
-        }
-        else {
-            itr = itr->next;
-        }
+        if(itr->val == itr->next->val) itr->next = itr->next->next;
+        else itr = itr->next;
     }
     return head;
 }
