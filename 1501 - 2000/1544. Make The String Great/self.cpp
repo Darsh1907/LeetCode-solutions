bd4024d8a32f2292db6bd545f9bcd1544e4941fb
Darsh Patel <darshpatelisin@gmail.com>
2024-04-05 08:04:17 +0530

    Update self.cpp

--- 1501 - 2000/1544. Make The String Great/self.cpp
+++ 1501 - 2000/1544. Make The String Great/self.cpp
@@ -11,7 +11,7 @@ public:
         string ans;
         ans.push_back(s[0]);
         for(int i=1; i<n; i++){
-            if(!ans.empty() && s[i]!=ans.back() && abs(s[i]-ans.back())==32) ans.pop_back();
+            if(!ans.empty() && abs(s[i]-ans.back())==32) ans.pop_back();
             else ans.push_back(s[i]);
         }
         return ans;
