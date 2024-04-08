f3afaef9ef04335ce2914c52e11910df12c23489
Darsh Patel <darshpatelisin@gmail.com>
2024-04-08 11:21:22 +0530

    solve 1700

---
+++ 1501 - 2000/1700. Number of Students Unable to Eat Lunch/soln.c
@@ -0,0 +1,13 @@
+int countStudents(int* students, int studentsSize, int* sandwiches, int sandwichesSize) {
+    int size = studentsSize;
+    int counts[2] = {0}; // {countof0, countof1}
+    for(int i=0; i<size; i++) counts[students[i]]++;
+    int remaining = size;
+    for(int i=0; i<size; i++) {
+        if(counts[sandwiches[i]] == 0) break;
+        if(remaining == 0) break;
+        counts[sandwiches[i]]--;
+        remaining--;
+    }
+    return remaining;
+}
