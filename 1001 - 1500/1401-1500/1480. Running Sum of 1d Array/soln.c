1b45f0db12755800b1fb4a0ddd9d6b71b34e1cfb
Darsh Patel <darshpatelisin@gmail.com>
2024-04-23 22:57:03 +0530

    Update soln.c

--- 1001 - 1500/1401-1500/1480. Running Sum of 1d Array/soln.c
+++ 1001 - 1500/1401-1500/1480. Running Sum of 1d Array/soln.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
- */
+*/
+
 int* runningSum(int* nums, int numsSize, int* returnSize){
     if (numsSize <= 1) {
         *returnSize = numsSize;
