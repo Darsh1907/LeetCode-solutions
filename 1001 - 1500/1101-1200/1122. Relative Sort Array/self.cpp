4634ef1f4b2cc0e05731b13805ba342e396dad35
Darsh Patel <darshpatelisin@gmail.com>
2024-06-11 09:13:16 +0530

    solve 1122

---
+++ 1001 - 1500/1101-1200/1122. Relative Sort Array/self.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    std::vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int> res;
+        map<int, int> arr1map;
+        for(int i=0; i<arr1.size(); i++) arr1map[arr1[i]]++;
+
+        for(int i=0; i<arr2.size(); i++) {
+            auto ptr = arr1map.find(arr2[i]);
+            int count = ptr->second;
+            while(count--) res.push_back(arr2[i]);
+            ptr->second = 0;
+            arr1map.erase(ptr);
+        }
+
+        for(auto i=arr1map.begin(); i!=arr1map.end(); i++) {
+            int count = i->second;
+            while(count--) res.push_back(i->first);
+        }
+
+        return res;
+    }
+};
