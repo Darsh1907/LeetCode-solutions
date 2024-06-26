f7546ae2915c2c28a8a44d20b7d3013ac4aacb5d
Darsh Patel <darshpatelisin@gmail.com>
2024-06-26 22:28:41 +0530

    Update optimised.cpp

--- 0001 - 0500/001-100/026-050/49. Group Anagrams/optimised.cpp
+++ 0001 - 0500/001-100/026-050/49. Group Anagrams/optimised.cpp
@@ -3,12 +3,12 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, int> mp;
         vector<vector<string>> ans; 
-        for (int i = 0; i < strs.size(); i++) {
+        for (int i=0; i<strs.size(); i++) {
             string temp = strs[i];
             sort(temp.begin(), temp.end());
-            if(mp.find(temp) != mp.end()) ans[mp[temp]].push_back(strs[i]);
+            if(mp.find(temp) != mp.end()) ans[mp[temp]].push_back(strs[i]); // get index for the already existing anagram and store the new word there.
             else {
-                mp[temp] = ans.size();
+                mp[temp] = ans.size(); // creating new index
                 ans.push_back({strs[i]}); 
             }
         }
