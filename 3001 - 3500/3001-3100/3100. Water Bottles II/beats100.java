512ca7363074abe1c1da2361bf2c75f8cc139974
Darsh Patel <darshpatelisin@gmail.com>
2024-06-28 20:08:38 +0530

    Create beats100.java

---
+++ 3001 - 3500/3001-3100/3100. Water Bottles II/beats100.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int ans = numBottles;
+        int total = numBottles;
+        while(numExchange <= total ) {
+            total -= numExchange;
+            total++;
+            numExchange++;
+            ans++;
+        }
+        return ans;
+    }
+}
