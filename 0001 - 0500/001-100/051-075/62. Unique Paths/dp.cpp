3f60013af55acb786b944a8c14fdf4d289f66ffb
Darsh Patel <darshpatelisin@gmail.com>
2024-07-07 19:23:06 +0530

    Update dp.cpp

--- 0001 - 0500/001-100/051-075/62. Unique Paths/dp.cpp
+++ 0001 - 0500/001-100/051-075/62. Unique Paths/dp.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int uniquePathsUtil(int m, int n) {
-        if(m == 1 || n == 1) return 1;
-        if(dp[m][n] != -1)
-            return dp[m][n];
-        return dp[m][n] = uniquePathsUtil(m-1, n) + uniquePathsUtil(m, n-1);
+    int uniquePathsUtil(int m, int n, vector<vector<int>>& dp) {
+        if(m==1 || n==1) return 1;
+        if(dp[m][n] != -1) return dp[m][n];
+        return dp[m][n] = uniquePathsUtil(m-1, n, dp) + uniquePathsUtil(m, n-1, dp);
     }
     int uniquePaths(int m, int n) {
-        dp.clear();
-        dp.assign(m+1, vector<int>(n+1, -1));
-        return uniquePathsUtil(m, n);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return uniquePathsUtil(m, n, dp);
     }
 };
