240f117366b8b77f0f34f957b5111d4cb97017e1
Darsh Patel <darshpatelisin@gmail.com>
2024-07-14 12:37:55 +0530

    Update self.cpp

--- 3001 - 3500/3216. Lexicographically Smallest String After a Swap/self.cpp
+++ 3001 - 3500/3216. Lexicographically Smallest String After a Swap/self.cpp
@@ -1,13 +1,8 @@
 class Solution {
-    bool hasSameParity(int a, int b) {
-        if(a%2==0 && b%2==0) return true;
-        if(a%2==1 && b%2==1) return true;
-        return false;
-    }
 public:
     string getSmallestString(string s) {
         for(int i=1; i<s.size(); i++) {
-            if(hasSameParity(s[i], s[i-1]) && s[i]<s[i-1]) {
+            if((s[i]%2)==(s[i-1]%2) && s[i]<s[i-1]) {
                 swap(s[i], s[i-1]);
                 break;
             }
