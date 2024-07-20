827774decd085f71269e70c8d73ac6ebc520086d
Darsh Patel <darshpatelisin@gmail.com>
2024-07-21 00:15:17 +0530

    solve 1605

---
+++ 1501 - 2000/1601-1700/1605. Find Valid Matrix Given Row and Column Sums/soln.cpp
@@ -0,0 +1,21 @@
+// reference-amit: https://www.youtube.com/watch?v=jWTcK-BaLEk
+// reference-neetcode: https://www.youtube.com/watch?v=Ks6fGnXkHPg
+
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int numRows = rowSum.size();
+        int numCols = colSum.size();
+        vector<vector<int>> result(numRows, vector<int>(numCols, 0));
+        int i = 0, j = 0;
+        while (i < numRows && j < numCols) {
+            int val = min(rowSum[i], colSum[j]);
+            result[i][j] = val;
+            rowSum[i] -= val;
+            colSum[j] -= val;
+            if (rowSum[i] == 0) i++;
+            if (colSum[j] == 0) j++;
+        }
+        return result;
+    }
+};
