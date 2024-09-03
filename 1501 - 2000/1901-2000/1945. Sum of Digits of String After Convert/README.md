2ecda022752842f54d596b3c196f5308d6242b5e
Darsh Patel <darshpatelisin@gmail.com>
2024-09-03 10:03:04 +0530

    solve 1945

---
+++ 1501 - 2000/1901-2000/1945. Sum of Digits of String After Convert/README.md
@@ -0,0 +1,5 @@
+# 1945. Sum of Digits of String After Convert
+## (Easy)
+
+![alt text](image.png)
+![alt text](image-1.png)
---
+++ 1501 - 2000/1901-2000/1945. Sum of Digits of String After Convert/soln.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        // Convert each character in the string to its corresponding numeric value
+        string number;
+        for (char ch : s) {
+            number += to_string(ch - 'a' + 1);
+        }
+        // Perform the transformation `k` times
+        while (k > 0) {
+            int temp = 0;
+            for(char x : number) temp += x-'0';  // Sum the digits of the current number
+            number = to_string(temp);  // Convert the sum back to a string
+            k--;
+        }
+        return stoi(number);  // Return the final result as an integer
+    }
+};
