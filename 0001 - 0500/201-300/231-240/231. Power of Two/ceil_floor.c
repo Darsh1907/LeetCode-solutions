494ecdf9b7e898c91eccf8a4be07e0c0d000ecf2
Darsh Patel <darshpatelisin@gmail.com>
2024-09-24 10:56:12 +0530

    changes in 231

--- 0001 - 0500/201-300/231-240/231. Power of Two/ceil_floor.c
+++ 0001 - 0500/201-300/231-240/231. Power of Two/ceil_floor.c
@@ -1,11 +1,3 @@
-/*
-We are literally gonna take the advantage of the constraints. 
-I dont recommend you all to use this method during your interviews. 
-It's a fun method . Do this on your own, you will enjoy it ;))
-*/
-
-// Hint: Constraints [-2^31 <= n <= 2^31 - 1] also 2^30 is 1073741824
-
 bool isPowerOfTwo(int n) {
     if(n<=0) return false;
     return ceil(log2(n)) == floor(log2(n));
