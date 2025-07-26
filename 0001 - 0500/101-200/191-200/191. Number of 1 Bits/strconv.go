37c70ed61d164ecef84d6f4ec3897706380ac78d
Darsh Patel <darshpatelisin@gmail.com>
2025-07-26 09:53:59 +0530

    add main function to demonstrate hammingWeight usage

--- 0001 - 0500/101-200/191-200/191. Number of 1 Bits/strconv.go
+++ 0001 - 0500/101-200/191-200/191. Number of 1 Bits/strconv.go
@@ -1,3 +1,9 @@
+package main
+
+import (
+	"strconv"
+)
+
 func hammingWeight(n int) int {
 	res := 0
 	binary := strconv.FormatInt(int64(n), 2)
@@ -7,4 +13,11 @@ func hammingWeight(n int) int {
 		}
 	}
 	return res
+}
+
+func main() {
+	// Example usage
+	n := 11 // Binary: 1011, Hamming weight: 3
+	result := hammingWeight(n)
+	println("Hamming weight of", n, "is", result) // Output: Hamming weight of 11 is 3
 }
