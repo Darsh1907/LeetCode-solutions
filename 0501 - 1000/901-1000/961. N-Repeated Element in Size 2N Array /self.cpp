c98704d7c291176f68b925210b8317c850debe98
Darsh Patel <118650412+Darsh1907@users.noreply.github.com>
2026-01-02 21:32:23 +0530

    Implement solution for N-Repeated Element problem

---
+++ 0501 - 1000/901-1000/961. N-Repeated Element in Size 2N Array /self.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int num: nums) {
+            if(s.find(num) != s.end()) return num;
+            s.insert(num);
+        }
+        return -1;
+    }
+};
