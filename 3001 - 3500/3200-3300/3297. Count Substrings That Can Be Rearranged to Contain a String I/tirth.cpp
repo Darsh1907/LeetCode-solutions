a2ffee84eff5a6a018f802980e6f46a5c75b8374
Darsh Patel <darshpatelisin@gmail.com>
2026-08-23 17:14:45 +0530

    comments

--- 3001 - 3500/3200-3300/3297. Count Substrings That Can Be Rearranged to Contain a String I/tirth.cpp
+++ 3001 - 3500/3200-3300/3297. Count Substrings That Can Be Rearranged to Contain a String I/tirth.cpp
@@ -10,16 +10,16 @@ public:
     long long validSubstringCount(string word1, string word2) {
         int n = word1.length();
         int m = word2.length();
-        vector<int> target(26, 0);
+        vector<int> target(26, 0); // target will hold the required count of each character
         for(char c : word2) target[c - 'a']++;
         long long result = 0;
-        vector<int> current(26, 0);
+        vector<int> current(26, 0); // current holds the current count of chars in the selected substring
         int left = 0;
         for (int right = 0; right < n; right++) {
             current[word1[right] - 'a']++;
-            while(left<=right && isValid(current, target)) {
-                result += n - right;
-                current[word1[left] - 'a']--;
+            while(left<=right && isValid(current, target)) { // if the substring has all valid chars
+                result += n - right; // count all possible number of substrings possible within that substrings
+                current[word1[left] - 'a']--; // move left
                 left++;
             }
         }
